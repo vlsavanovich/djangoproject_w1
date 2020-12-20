7b9ccadfa8508f68676956305542e37113c00758
vlsavanovich <73171654+vlsavanovich@users.noreply.github.com>
2020-12-20 19:10:10 +0300

    Update settings.py
    DEBUG выключил)

--- stepik_tours/stepik_tours/settings.py
+++ stepik_tours/stepik_tours/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2y27vq!_)ka^nskqex(*6m9r#*#f^o_by%(+(jpm4hr(_z98@4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
