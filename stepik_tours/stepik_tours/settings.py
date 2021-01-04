ae528231a60d45240be196b8f655be980c0f1c9a
vlsavanovich <farmfilok@gmail.com>
2021-01-04 23:06:13 +0300

    debug true

--- stepik_tours/stepik_tours/settings.py
+++ stepik_tours/stepik_tours/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2y27vq!_)ka^nskqex(*6m9r#*#f^o_by%(+(jpm4hr(_z98@4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
